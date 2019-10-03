da9aa1204d6564f5414bde8fd9ea007152402dc7
jiang yinzuo <392711804@qq.com>
2019-10-03 22:11:53 +0800

    initial commit

---
+++ src/main/java/pers/jyz/bootlaunch2/BootLaunch2Application.java
@@ -0,0 +1,13 @@
+package pers.jyz.bootlaunch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLaunch2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLaunch2Application.class, args);
+    }
+
+}
